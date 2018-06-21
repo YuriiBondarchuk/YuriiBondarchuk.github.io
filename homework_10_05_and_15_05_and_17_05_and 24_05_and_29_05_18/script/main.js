5c484ca062eac6c2c3690e5d54f88c02438537e6
YuriiBondarchuk <ymbondarchuk@gmail.com>
2018-06-21 17:14:20 +0300

    added homework from 24_05&29_05_18

--- homework_10_05_and_15_05_and_17_05_and 24_05_and_29_05_18/script/main.js
+++ homework_10_05_and_15_05_and_17_05_and 24_05_and_29_05_18/script/main.js
@@ -1,5 +1,133 @@
 var app = angular.module('app', []);
 
+//Список добавленых в  корзину товаров
+app.factory('Basket',function () {
+    var Factory = function () {
+        var products = [];
+
+        this.get = function () { return products; };
+        this.add = function (v) { products.push(v); };
+        this.reset = function () { products = []; };
+    };
+    return Factory;
+});
+
+// контроллер корзины homework from 24/05/18 and 29/05/18
+app.controller('BasketController', function ($rootScope,$scope) {
+
+    this.getBasket = function () {
+
+        return $rootScope.basket;
+    }
+    this.getBasketMessage = function () {
+
+        return $rootScope.basketmessage;
+    }
+    this.getList = function () {
+        return $rootScope.list;
+    }
+    $rootScope.show = false
+    console.log($rootScope.show);
+    $scope.BasketShow = function () {
+
+        $rootScope.show = true;
+       console.log($rootScope.show);
+    }
+
+});
+/*Конец*/
+
+//Контроллер промо кода homework from 24/05/18 and 29/05/18
+
+app.controller('PromoController', function ($scope, $rootScope) {
+    this.getPromo = function () {
+
+        return $rootScope.promoadd;
+    }
+
+
+    $scope.addPromo = function () {
+        $rootScope.promoadd = +$scope.promokod;
+        if ($rootScope.promoadd == 1234) {
+            $rootScope.basket *= 2;
+        }
+        $scope.promokod = '';
+    }
+});
+/*Конец*/
+/* ********************************* */
+
+/* Модальное окно и сопутсвующий код homework from 24/05/18 and 29/05/18 */
+
+app.factory('notifications', function () {
+    var message = 'Вы дейсвительно хотите купить товар ?';
+    var showmodal = false;
+    return {
+        get: function () {
+            return message;
+        },
+        hide: function () {
+            return showmodal
+        },
+        show: function (value) {
+            return showmodal = value;
+        },
+        close: function () {
+            return showmodal = false;
+        }
+    };
+});
+
+app.controller('ModalController', function ($scope, notifications, $rootScope) {
+    this.hideModal = notifications.hide;
+    this.getModal = notifications.get();
+    this.closeModal = notifications.close;
+
+
+    $rootScope.basket = 0;
+    $rootScope.basketmessage = 'Нет товаров';
+
+    this.yesBasket = function (event) {
+        $rootScope.basket++;
+        $rootScope.basketmessage = ' товар(а/ов)';
+        notifications.close();
+    }
+
+});
+app.controller("SomeController", function ($scope,notifications,Basket,$rootScope) {
+    $scope.someClick = function ($event) {
+        notifications.show(event);
+
+
+    }
+    this.getMethod = function () {
+        return $rootScope.list.get();
+    }
+
+    $scope.Clear = function () {
+        $rootScope.list.reset();
+        $rootScope.basket = 0;
+        $rootScope.basketmessage = 'Нет товаров';
+        $rootScope.show = false;
+    }
+
+
+   $scope.List = function () {
+      $rootScope.list.get();
+   }
+    $scope.ShowBasket = function () {
+        return $rootScope.show;
+    }
+
+   $scope.ExitBasket = function () {
+         $rootScope.show = false;
+
+   }
+
+
+})
+/*Конец*/
+/* ********************************* */
 
 
 var random = {
@@ -48,16 +176,24 @@ app.controller('GalleryController', function () {
 });
 
 app.controller('ReviewController', function () {
-    this.colors = ['black','white','yellow','green'];
+    this.colors = ['black', 'white', 'yellow', 'green'];
     this.review = {};
     this.arr = [];
-    this.addReview = function(color){
-       return this.arr.push(color);
-       };
+    this.addReview = function (color) {
+        return this.arr.push(color);
+    };
 
 });
 
-app.controller('ProductsController', function () {
+app.controller('ProductsController', function (notifications, $scope,Basket,$rootScope) {
+     //Модалка ng-show и добавление товаров в корзину
+     $rootScope.list = new Basket;
+
+    $scope.someClick = function ($event) {
+        notifications.show(event);
+        $rootScope.list.add($event);
+
+    }
 
 
 

676132ff1f2390436c6a34268e2fff06351d3b9a
YuriiBondarchuk <ymbondarchuk@gmail.com>
2018-07-06 13:01:49 +0300

    homework path3-4

---
+++ homework_by_React/path_3/src/Sneakers.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+
+
+class Tovar_page extends Component {
+
+
+    render() {
+
+
+
+
+
+        return (
+           <h1>Hello</h1>
+
+        )
+    }
+}
+
+export default Tovar;
